c8b62e5ac417e229263a2b1711d2246c5bb61ac1
gug <incisaguglielmo@hotmail.com>
2021-12-01 20:14:13 +0000

    Initial Commit

---
+++ NetDrain/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  NetDrain
+//
+//  Created by Guglielmo Incisa di Camerana on 01/12/2021.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
