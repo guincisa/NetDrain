afd0b4ecab27bf8112534ad3c3afe290f2d20403
gug <incisaguglielmo@hotmail.com>
2021-12-26 22:13:18 +0000

    more

--- NetDrain/ENG.hpp
+++ NetDrain/ENG.hpp
@@ -31,14 +31,20 @@
 #include <condition_variable> // std::condition_variable
 #include <queue>
 #include <unistd.h>
+#include <iostream>
+#include <stdexcept>
+#include <stdio.h>
+#include <string>
+
 using namespace std;
 
 
 class TH{
     public:
-     static mutex mtx, mtxQ, stop;
+     static mutex mtx, mtxQ;
      static condition_variable cv;
      static queue<void*> siteq;
+     static string* _website;
 
      static void runSite (int thid) {
       while(true){
@@ -47,24 +53,46 @@ class TH{
               cout<<thid<<" waiting"<<endl;
               cv.wait(lck);
           }
+          cout << "gotcha" <<endl;
           mtxQ.lock();
-          int z = *((int*)siteq.front());
+          string* z = (string*)siteq.front();
           siteq.pop();
           mtxQ.unlock();
 
 
-          cout << ">>>DOING" << z <<".." << thid<< endl;
+          cout << " >>>DOING string " << *z <<" "<< thid <<endl;
           lck.unlock();
-          usleep(400000);
-          cout << "<<<DONE"<< z <<".." << thid<< endl;
+          exec("wget "+*z);
+          cout << " >>>DONE string " << *z <<" "<< thid <<endl;
 
           }
       }
+    
+    static string exec(string command) {
+       char buffer[128];
+       string result = "";
+
+       // Open pipe to file
+       FILE* pipe = popen(command.c_str(), "r");
+       if (!pipe) {
+          return "popen failed!";
+       }
+
+       // read till end of process:
+       while (!feof(pipe)) {
+
+          // use buffer to read and add to result
+          if (fgets(buffer, 128, pipe) != NULL)
+             result += buffer;
+       }
+
+       pclose(pipe);
+       return result;
+    }
 };
 condition_variable TH::cv;
 mutex TH::mtx;
 mutex TH::mtxQ;
-mutex TH::stop;
 queue<void*> TH::siteq;
 
 class ENG{
@@ -82,22 +110,34 @@ public:
     ENG(){
         int MT=10;
 
-        std::vector<thread> threads(MT);
-        // spawn n threads:
-        for (int i = 0; i < MT; i++) {
-            threads[i] =  thread(TH::runSite, i+1);
-        }
-        unique_lock<mutex> lck(TH::stop);
-        usleep(400000);
-        int i = 1;
-        inq((void*)&i);
-        usleep(400000);
-        int ii = 2;
-        inq((void*)&ii);
+        //std::vector<thread> threads(MT);
+        std:thread t1(TH::runSite,1),t2(TH::runSite,2),t3(TH::runSite,3);
+        thread t4(TH::runSite,4),t5(TH::runSite,5),t6(TH::runSite,6);
 
-        TH::stop.lock();
+//        // spawn n threads:
+//        for (int i = 0; i < MT; i++) {
+//            threads[i] =  thread(TH::runSite, i+1);
+//        }
+        t1.join();
+        t2.join();
+        t3.join();
+        t4.join();
+        t5.join();
+        t6.join();
+
+        cout << "spwaned" <<endl;
 
     }
 
 };
+
+
+
+//int main() {
+//   cout << exec("wget www.apple.com");
+//   cout << exec("wget www.google.com");
+//   cout << exec("wget www.dell.com");
+//   cout <<  exec("wget www.nokia.com");
+//}
+
 #endif
--- NetDrain/main.cpp
+++ NetDrain/main.cpp
@@ -10,12 +10,47 @@
 #include "ENG.hpp"
 using namespace std;
 
+ENG* ee;
 
+void th2(){
+    ee = new ENG();
+}
 
 int main() {
     cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
 
-    ENG ee;
+    std:thread tENG (th2);
+    string* s1 = new string("www.apple.com");
+    string* s2 = new string("www.nokia.com");
+    string* s3 = new string("www.microsoft.com");
+    string* s4 = new string("www.ibm.com");
+    string* s5 = new string("www.google.com");
+    
+    cout << s1 << " " << *s1<<endl;
+    cout << s2 << " " << *s2<<endl;
+    cout << s3 << " " << *s3<<endl;
+    cout << s4 << " " << *s4<<endl;
+    cout << s5 << " " << *s5<<endl;
+
+    ee->inq((void*)s1);
+    ee->inq((void*)s2);
+    ee->inq((void*)s3);
+    ee->inq((void*)s4);
+    ee->inq((void*)s5);
+//
+    static mutex stop;
+    unique_lock<mutex> lck(stop);
+    stop.lock();
+    stop.lock();
+    cout << "dying" << endl;
+    
+    /*
+     
+     GET / HTTP/1.1
+     Host: www.example.com
+     User-Agent: Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1)
+
+     */
 
 
     return 0;
